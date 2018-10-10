fb48cc3db2013f9c0220bc979c4d421368ad5f28
Tony Huang <tonyhuang4326@gmail.com>
2018-10-10 01:12:23 +0000

    added rock paper scissors buttons, made them clickable, added scoreboard

--- js/script.js
+++ js/script.js
@@ -8,4 +8,23 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-
+$(document).ready(function(){
+    $("#rock").click(function(){
+        $("#rock").css("border","3px black solid");
+        $("#paper").css("border","0px black solid");
+        $("#scissors").css("border","0px black solid");
+    });
+    $("#paper").click(function(){
+        $("#rock").css("border","0px black solid");
+        $("#paper").css("border","3px black solid");
+        $("#scissors").css("border","0px black solid");
+    });
+    $("#scissors").click(function(){
+        $("#rock").css("border","0px black solid");
+        $("#paper").css("border","0px black solid");
+        $("#scissors").css("border","3px black solid");
+    });
+    $("button").click(function(){
+        
+    });
+})
