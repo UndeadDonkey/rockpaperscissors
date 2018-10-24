f213d58b102206d5b631e5c184b5e872a08f1f45
Tony Huang <tonyhuang4326@gmail.com>
2018-10-24 01:06:01 +0000

    Made scoring work, made a ton of conditional statements in conditional statements

--- js/script.js
+++ js/script.js
@@ -16,40 +16,71 @@ $(document).ready(function(){
     var scissors = "https://images-na.ssl-images-amazon.com/images/I/41o907KrqfL._SX425_.jpg";
     var paper = "https://neal.fun/paper/paper.jpg";
     var rock = "http://www.arriagastone.com/wp-content/uploads/2017/03/ARRIAGA-STONE.jpg";
+    var userScore = 0;
+    var computerScore= 0;
     
     $("#rock").click(function(){
         $("#rock").css("border","3px black solid");
         $("#paper").css("border","0px black solid");
         $("#scissors").css("border","0px black solid");
-        userChoice = "http://www.arriagastone.com/wp-content/uploads/2017/03/ARRIAGA-STONE.jpg";
+        userChoice = rock;
     });
     $("#paper").click(function(){
         $("#rock").css("border","0px black solid");
         $("#paper").css("border","3px black solid");
         $("#scissors").css("border","0px black solid");
-        userChoice = "https://neal.fun/paper/paper.jpg";
+        userChoice = paper;
     });
     $("#scissors").click(function(){
         $("#rock").css("border","0px black solid");
         $("#paper").css("border","0px black solid");
         $("#scissors").css("border","3px black solid");
-        userChoice = "https://images-na.ssl-images-amazon.com/images/I/41o907KrqfL._SX425_.jpg";
+        userChoice = scissors;
     });
     $("button").click(function(){
         randomNumber = Math.random();
         if(randomNumber < 0.33333){
-            $("#computerChoice").attr({"src":"http://www.arriagastone.com/wp-content/uploads/2017/03/ARRIAGA-STONE.jpg","width":"33%"});
+            $("#computerChoice").attr({"src":rock,"width":"250px"});
         }else if(randomNumber >= 0.33333 && randomNumber < 0.66666 ) {
-            $("#computerChoice").attr({"src":"https://neal.fun/paper/paper.jpg","width":"33%"});
+            $("#computerChoice").attr({"src":paper,"width":"250px"});
         }else {
-            $("#computerChoice").attr({"src":"https://images-na.ssl-images-amazon.com/images/I/41o907KrqfL._SX425_.jpg","width":"33%"});
+            $("#computerChoice").attr({"src":scissors,"width":"250px"});
         }
-        $("#userChoice").attr({"src": userChoice,"width":"33%"});
         
+        $("#userChoice").attr({"src": userChoice,"width":"250px"});
+    
         if($("#userChoice").attr("src") === $("#computerChoice").attr("src")) {
-            
-        }else if($("#userChoice").attr("src")){
-            
+            $("#who").text("tie");
+        }else if($("#userChoice").attr("src") === scissors){
+            if($("#computerChoice").attr("src") === paper){
+                $("#who").text("Winner!");
+                userScore = userScore +1;
+                $("#userScore").html(userScore);
+            }else if($("#computerChoice").attr("src") === rock){
+                $("#who").text("Loser!");
+                computerScore = computerScore +1;
+                $("#computerScore").html(computerScore);
+            }
+        }else if($("#userChoice").attr("src") === paper){
+            if($("#computerChoice").attr("src") === scissors){
+                $("#who").text("Loser!");
+                computerScore = computerScore +1;
+                $("#computerScore").html(computerScore);
+            }else if($("#computerChoice").attr("src") === rock){
+                $("#who").text("Winner!");
+                userScore = userScore +1;
+                $("#userScore").html(userScore);
+            }
+        }else if($("#userChoice").attr("src") === rock){
+            if($("#computerChoice").attr("src") === scissors){
+                $("#who").text("Winner!");
+                userScore = userScore +1;
+                $("#userScore").html(userScore);
+            }else if($("#computerChoice").attr("src") === paper){
+                $("#who").text("Loser!");
+                computerScore = computerScore +1;
+                $("#computerScore").html(computerScore);
+            }
         }
     });
 })
